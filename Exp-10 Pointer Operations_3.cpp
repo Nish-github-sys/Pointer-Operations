3e8d75de063c00352ee1baef93f5075c8135634c
Nishka Ranadive <nishka.amol.aug2006@gmail.com>
2025-08-12 21:52:01 +0530

    Update and rename exp-10_3.cpp to Exp-10 Pointer Operations_3.cpp

--- Exp-10 Pointer Operations_3.cpp
+++ Exp-10 Pointer Operations_3.cpp
@@ -3,6 +3,7 @@
 //Exp-10
 
 //Program-3
+
 //Pass by Reference
 #include<iostream>
 using namespace std;
@@ -27,4 +28,11 @@ int main(){
     cout<<"Swapped Values are: "<<a<<" & "<<b;
 
     return 0;
+
 }
+
+
+/*==========OUTPUTS==========
+Values before swapping are: 5 & 10
+Swapped Values are: 10 & 5
+*/
